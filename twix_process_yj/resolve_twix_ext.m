39d8e3f7164f8204f62ad8da39164681c09b5aeb
Evelyn92 <Evelynjia92@gmail.com>
2025-01-14 11:41:42 +0100

    update Jan 14

--- twix_process_yj/resolve_twix_ext.m
+++ twix_process_yj/resolve_twix_ext.m
@@ -1,24 +1,42 @@
 clc; clear all;
-twix = load('/Users/cag/Documents/Dataset/MREyeTrack/Twix/twix_subj1_meas_MID00605_FID182859_BEAT_LIBREon_eye_(23_09_24).mat');
+subject_num = 2;
+
+if subject_num == 1
+    rawdata_name = 'meas_MID00605_FID182859_BEAT_LIBREon_eye_(23_09_24)';
+    twix = load(['/Users/cag/Documents/Dataset/MREyeTrack/Twix/Twix' ...
+        '/twix_subj1_meas_MID00605_FID182859_BEAT_LIBREon_eye_(23_09_24).mat']);
+    datadir = ['/home/debi/jaime/acquisitions/MREyeTrack/' ...
+    'MREyeTrack_subj1/RawData_MREyeTrack_Subj1/'];  
+elseif subject_num == 2
+    rawdata_name = 'meas_MID00580_FID182834_BEAT_LIBREon_eye_(23_09_24)';
+    twix = load(['/Users/cag/Documents/Dataset/MREyeTrack/Twix/Twix' ...
+        '/twix_subj2_meas_MID00580_FID182834_BEAT_LIBREon_eye_(23_09_24).mat']);
+    datadir = ['/home/debi/jaime/acquisitions/MREyeTrack/' ...
+    'MREyeTrack_subj2/RawData_MREyeTrack_Subj2/'];  
+else
+    rawdata_name = 'meas_MID00554_FID182808_BEAT_LIBREon_eye_(23_09_24)';
+    twix = load(['/Users/cag/Documents/Dataset/MREyeTrack/Twix/Twix' ...
+        '/twix_subj3_meas_MID00554_FID182808_BEAT_LIBREon_eye_(23_09_24).mat']);
+    datadir = ['/home/debi/jaime/acquisitions/MREyeTrack/' ...
+    'MREyeTrack_subj3/RawData_MREyeTrack_Subj3/'];  
+end
+
 twix_image2 = twix.twix{1,2};
 rawEXT = twix_image2.PMUdata.raw.EXT;
 rawTimestamp = twix_image2.PMUdata.raw.EXT.TimeStamp;
 rawEXTData = twix_image2.PMUdata.raw.EXT.data;
 
-%%
+%
 stop_time_ms = (rawTimestamp(end)-rawTimestamp(1)) * 2.5;
 rawTime_ms = linspace(0, stop_time_ms, length(rawTimestamp));
 ext1 = double(rawEXTData(:,1));
 ext2 = double(rawEXTData(:,2));%remember to convert the data type, otherwise something wrong with rawTimestamp.
-%%
+%
 raw_pmu_ext = array2table([rawTimestamp'; rawTime_ms(:)' ; ext1(:)'; ext2(:)']', 'VariableNames', ...
     {'PMU_timestamp', 'PMU_time_ms', 'EXT1', 'EXT2'});
 
 
 %%
-rawdata_name = 'meas_MID00273_FID178443_BEAT_LIBREon_T2_eye_(23_09_24)';
-datadir = ['/Users/cag/Documents/Dataset/' ...
-    '20241002_MRIAcquisition_Yiwei/PrePilot_YiweiJia/Raw'];  
 rawdatafile = fullfile(datadir,strcat(rawdata_name, '.dat')); 
  
 twix_obj = mapVBVD_JH(rawdatafile);
@@ -47,21 +65,39 @@ disp('The time diff between start of EXT and the first mark of EXT:')
 diff_start_mark = (raw_pmu_ext.PMU_timestamp(1) - raw_pmu_mark.PMU_timestamp(1))*2.5
 disp('the first mark of EXT - the first mri measure:')
 disp(num2str(diff_start_mri - diff_start_mark))
-% The time diff between start of EXT and the first measurement of MRI:
-% 
-% diff_start_mri =
-% 
-%  -70.499999988824129/2.5 = 28
-% 
-% The time diff between start of EXT and the first mark of EXT:
-% 
-% diff_start_mark =
-% 
-%  -38.999999985098839
-% 
-% the first mark of EXT - the first mri measure:
-% -31.5
 
+%% Generate a cell report
+report={};
+report{1,1}='pmu_timestp_start'; report{1,2}=raw_pmu_ext.PMU_timestamp(1);
+report{2,1}='pmu_timestp_end'; report{2,2}=raw_pmu_ext.PMU_timestamp(end);
+report{3,1}='pmu_mark_start'; report{3,2}=raw_pmu_mark.PMU_timestamp(1);
+report{4,1}='pmu_mark_end'; report{4,2}=raw_pmu_mark.PMU_timestamp(end);
+report{5,1}='raw_pmu_ext'; report{5,2}=raw_pmu_ext;
+report{6,1}='raw_pmu_mark'; report{6,2}=raw_pmu_mark;
 
-% raw_pmu_ext.PMU_timestamp(1)  =   2.3334430.8
-% raw_pmu_mark.PMU_timestamp(1)= 2.3334446.4
+report{7,1}='mri_timestp_start'; report{7,2}=mriTimeStamp(1);
+report{8,1}='mri_timestp_end'; report{8,2}=mriTimeStamp(end);
+report{9,1}='mriTimeStamp'; report{9,2}=mriTimeStamp;
+%%
+report{10,1}='ET_trigger_dot'; 
+%
+if subject_num == 1
+    report{10,2}=498;
+    trigger_interval = 1989.4;
+elseif subject_num == 2
+    report{10,2}=497;
+    trigger_interval = 2006.6;
+elseif subject_num == 3
+    report{10,2}=496;
+    trigger_interval = 1994;
+end
+
+report{11,1}='ET_offset_first_dot_mriStart'; report{11,2}=trigger_interval - report{10,2} - (report{3,2}-report{1,2})*2.5;
+disp('The report cell is generated!')
+%%
+
+reportPath = ['/home/debi/jaime/acquisitions/MREyeTrack/Twix/' ...
+        'report_subj', num2str(subject_num),'.mat'];
+save(reportPath, 'report');
+disp('report has been saved here:')
+disp(reportPath)
